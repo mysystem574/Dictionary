4131e16a29a720cc47428403422ad5779efb7d58
mysystem574 <mysystem574@gmail.com>
2024-10-17 13:39:46 +0530

    Create adding_keyvalues_into_dict.py

---
+++ adding_keyvalues_into_dict.py
@@ -0,0 +1,27 @@
+'''
+The program takes a key-value pair and adds it to the dictionary. 
+Problem Solution:
+1. Take a key-value pair from the user and store it in separate variables.
+2. Declare a dictionary and initialize it to an empty dictionary.
+3. Use the update() function to add the key-value pair to the dictionary.
+4. Print the final dictionary.
+5. Exit.
+Sample I/O:
+12
+34
+Sample o/p:
+{12: 34}
+'''
+
+A={}
+a=int(input())
+b=int(input())
+A[a]=b
+A.update()
+print(A)
+
+'''
+12
+34
+{12: 34}
+'''
